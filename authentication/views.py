ce9cfec366999cbde294b404ddb4839bb3e6e61f
João Neto <joaoauvs@gmail.com>
2022-07-17 22:06:05 -0300

    Update views authentication

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+from django.views.decorators.csrf import csrf_protect
 
-# Create your views here.
+
+def login_user(request):
+    return render(request, 'authentication/login.html')
+
+@login_required(login_url='/login/')
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+@csrf_protect
+def submit_login(request):
+    if request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/orquestrador/')
+        else:
+            messages.error(request, 'Usuário/Senha inválidos. Favor tentar novamente.')
+    return redirect('/')
