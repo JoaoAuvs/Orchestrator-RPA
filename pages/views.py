bb1c48d85790e6f053665c13350e8cd49c3824aa
João Neto <joaoauvs@gmail.com>
2022-07-22 22:57:07 -0300

    Update Views

--- pages/views.py
+++ pages/views.py
@@ -53,14 +53,32 @@ def table_orquestrador(request):
     try:
         lista = Robo.objects.all().order_by('nome')
         dashboard = {}
+        qdt_falha = []
+        qdt_desativado = []
+        qdt_desenvolvimento = []
         grupos = []
         for grupo in request.user.groups.all(): grupos.append(grupo.name)
-        for robo in lista:
-            if robo.departamento.name in grupos or request.user.is_staff:
-                dashboard[robo] = robo
+        for indice, valor in enumerate(lista):
+            if valor.departamento.name in grupos or request.user.is_staff:
+                dashboard[indice] = {
+                    'id': valor.id,
+                    'nome': valor.nome,
+                    'nome_alternativo': valor.nome_alternativo,
+                    'departamento': valor.departamento.name,
+                    'vm': valor.vm.nome_vm,
+                    'hora_execucao': valor.hora_execucao,
+                    'status': valor.status,
+                }
             else:
                 pass
-        return render(request, "pages/orquestrador.html", {"robos": dashboard})
+            if valor.status == 'FALHA': qdt_falha.append(+1)
+            if valor.status == 'DESATIVADO': qdt_desativado.append(+1)
+            if valor.status == 'EM DESENVOLVIMENTO': qdt_desenvolvimento.append(+1)
+        return render(request, "pages/orquestrador.html", {
+            "robos": dashboard,
+            "lista_falhas": qdt_falha,
+            "lista_desativados": qdt_desativado,
+            })
     except:
         print("A resposta não chegou com o formato esperado.")
         return render(request, "pages/orquestrador.html")
