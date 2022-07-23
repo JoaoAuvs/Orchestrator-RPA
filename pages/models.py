49e77db6587a6b42322be47d9ee4fb96cb72970d
João Neto <joaoauvs@gmail.com>
2022-07-22 22:57:19 -0300

    Update Models

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.contrib.auth.models import Group, User
 from django.db import models
 
@@ -10,12 +12,19 @@ class Vm(models.Model):
         return self.nome_vm
 
 class Robo(models.Model):
-    nome = models.CharField(max_length=100)
-    nome_alternativo = models.CharField(max_length=100)
+    class Status(models.TextChoices):
+        OPERACIONAL = 'OPERACIONAL', 'OPERACIONAL'
+        EXECUTANDO = 'EXECUTANDO', 'EXECUTANDO'
+        CONCLUIDO = 'CONCLUÍDO', 'CONCLUÍDO'
+        FALHA = 'FALHA', 'FALHA'
+        DESATIVADO = 'DESATIVADO', 'DESATIVADO'
+
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    nome_alternativo = models.CharField(max_length=100, null=False, blank=False)
     departamento = models.ForeignKey(Group, on_delete=models.PROTECT)
     vm = models.ForeignKey(Vm, on_delete=models.PROTECT)
-    hora_execucao = models.DateTimeField(blank=True, null=True, editable=False)
-    status = models.CharField(max_length=100, default='OPERACIONAL')
+    hora_execucao = models.DateTimeField(null=False, blank=False,default= datetime.now)
+    status = models.CharField(max_length=200, choices=Status.choices, default=Status.OPERACIONAL)
 
     def __str__(self):
         return self.nome
